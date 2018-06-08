4ad443ba7d0544ee8926aec934f91de17146b6fc
RageBill <kelvin@firstcodeacademy.com>
2018-06-08 13:04:28 +0800

    Production build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 import JokeGenerator from './components/JokeGenerator/JokeGenerator';
 
 class App extends Component {
